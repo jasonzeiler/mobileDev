fcb65db7216056e8bc3e42694f532e15a4b111cb
Jason Zeiler <jasonzeiler@users.noreply.github.com>
2014-09-08 16:57:28 -0500

    update to homework1

--- homework/homework1.playground/section-1.swift
+++ homework/homework1.playground/section-1.swift
@@ -2,4 +2,11 @@
 
 import UIKit
 
-var str = "Hello, playground"
+let viewRect = CGRect(x: 0.0, y: 0.0, width: 320.0, height: 480.0)
+let view = UIView(frame: viewRect)
+
+//let view = UIView(frame: viewRect)
+//view.backgroundColor = UIColor.blackColor()
+view.backgroundColor = UIColor(red: 240.0/255.0, green: 230.0/255.0, blue: 242.0/255.0, alpha: 1.0)
+
+
