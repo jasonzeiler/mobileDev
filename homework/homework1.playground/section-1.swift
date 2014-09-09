a73f2dbc5a52a3862b8412654b4ef18b5480204a
Jason Zeiler <jasonzeiler@users.noreply.github.com>
2014-09-08 19:18:06 -0500

    updated green subview

--- homework/homework1.playground/section-1.swift
+++ homework/homework1.playground/section-1.swift
@@ -10,12 +10,13 @@ let view = UIView(frame: viewRect)
 view.backgroundColor = UIColor(red: 240.0/255.0, green: 230.0/255.0, blue: 242.0/255.0, alpha: 1.0)
 
 let greenView = UIView(frame: CGRectZero)
+greenView.setTranslatesAutoresizingMaskIntoConstraints(false)
 greenView.backgroundColor = UIColor.greenColor()
 
 view.addSubview(greenView)
 
 let greenViewTopConstraint = NSLayoutConstraint(item: greenView, attribute: .Top, relatedBy: .Equal, toItem: view, attribute: .Top, multiplier: 1.0, constant: 20.0)
-let greenViewRightConstraint = NSLayoutConstraint(item: greenView, attribute: .Top, relatedBy: .Equal, toItem: view, attribute: .Top, multiplier: 1.0, constant: -16.0)
+let greenViewRightConstraint = NSLayoutConstraint(item: greenView, attribute: .Right, relatedBy: .Equal, toItem: view, attribute: .Right, multiplier: 1.0, constant: -16.0)
 let greenViewBottomConstraint = NSLayoutConstraint(item: greenView, attribute: .Bottom, relatedBy: .Equal, toItem: view, attribute: .Bottom, multiplier: 1.0, constant: -10.0)
 let greenViewLeftConstraint = NSLayoutConstraint(item: greenView, attribute: .Left, relatedBy: .Equal, toItem: view, attribute: .Left, multiplier: 1.0, constant: 16.0)
 
@@ -36,3 +37,9 @@ yellowView.backgroundColor = UIColor.yellowColor()
 view.addSubview(blueView)
 view.addSubview(orangeView)
 view.addSubview(yellowView)
+
+var constraints = [greenViewBottomConstraint, greenViewTopConstraint, greenViewLeftConstraint, greenViewRightConstraint]
+
+view.addConstraints(constraints)
+view.layoutIfNeeded()
+view
